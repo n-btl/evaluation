d76a21f29cb75ee7fb407b0ed95df9efbf8f6191
noah.btl <noahboitel28@gmail.Com>
2025-10-06 09:59:37 +0200

    mise a jour

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Happy developing ✨')
+const input= document.getElementById("nom");
+const zone= document.getElementById("message");
+const bouton= document.getElementById("valider");
+bouton.addEventListener("click", function () {
+    zone.innerHTML="Bonjour "+input.value+"";
+    console.log("Bonjour "+input.value);
+    if(input.value==""){
+
+    }
+});
